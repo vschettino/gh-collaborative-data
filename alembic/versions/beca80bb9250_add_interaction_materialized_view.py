dd0134ae53c4ae8078d0df7dc7c8381244cabb1b
Vinicius Schettino <schettino.viniciusjunqueira@gmail.com>
2021-04-13 16:48:44 -0300

    Add migration to interactions view

---
+++ alembic/versions/beca80bb9250_add_interaction_materialized_view.py
@@ -0,0 +1,41 @@
+"""Add Interaction materialized view
+
+Revision ID: beca80bb9250
+Revises: 4479d18b570c
+Create Date: 2021-04-13 13:50:16.107383
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "beca80bb9250"
+down_revision = "4479d18b570c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        """
+        CREATE MATERIALIZED VIEW interactions AS
+            SELECT url, user_id, repository_id, created_at, 'Issue' as type
+            FROM issues
+        UNION
+            select url, user_id, repository_id, created_at, 'Pull Request'
+            from pull_requests
+        UNION
+            select prc.url, prc.user_id, pr.repository_id, prc.created_at, 'Issue Comment'
+            from pull_request_comments prc
+                     INNER JOIN pull_requests pr on pr.id = prc.pull_request_id
+        UNION
+            select prc.url, prc.user_id, pr.repository_id, prc.created_at, 'Pull Request Comment'
+            from pull_request_comments prc
+                     INNER JOIN pull_requests pr on pr.id = prc.pull_request_id
+        """
+    )
+
+
+def downgrade():
+    pass
